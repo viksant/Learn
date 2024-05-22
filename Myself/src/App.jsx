b3d4a1cc866d4ce471aadbfd67d583c21d3e88a3
VIKSANT <viksantviksant@gmail.com>
2024-05-22 13:43:18 +0200

    3 en raya

--- Myself/src/App.jsx
+++ Myself/src/App.jsx
@@ -1,39 +1,14 @@
-import './App.css';
 import { useState } from 'react'
-const TURNS = {
-  X: '❌',
-  O: '⚪'
-};
-
-
-const Square = ({ children, isSelected, updateBoard, index}) => {
-  const className = `square ${isSelected ? 'is-selected' : ''}`
-
-  const handleclick = () => {
-    updateBoard(index)
-  }
-  return (
-    <div onClick={handleclick} className={className}>
-      {children}
-    </div>
-  )
-}
-
-const WINNER_COMBOS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Square } from './Objects'
+import { TURNS, WINNER_COMBOS } from './Constants'
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [winner, setWinner] = useState(null) // No hay ganador. False es empate.
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(Array(9).fill(null)) //<-- Estado inicial
+  const [turn, setTurn] = useState(TURNS.X) 
+  const [winner, setWinner] = useState(null) // Null = No hay ganador, False = empate.
 
   const checkWinner = (boardToCheck) => {
     for (const combo of WINNER_COMBOS) {
@@ -50,39 +25,40 @@ function App() {
     // No hay gannador
     return null
   }
-
   const checkEndGame = (boardToCheck) => {
     return boardToCheck.every((square) => square !== null);
   }
-  
+
+  const updateBoard = (index) => {
+    // Return si la celda ya tiene algo dentro.
+    if (board[index] || winner) return 
+    // Creamos copia del board para actualizarlo.
+    const newBoard = [...board] 
+    // Obtiene lo que hay dentro de la celda (X u O). 
+    newBoard[index] = turn 
+    setBoard(newBoard) 
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    // Comprobar si hay un ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
+    }
+  }
+
   const resetGame = () => {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
   }
 
-  const updateBoard = (index) => {
-    // No dejar cambiar un cuadrado que ya tenga valor o existe un ganador.
-    if (board[index] || winner) return
-
-    // Hacemos una copia del array para evitar modificar el original.
-    const newBoard = [...board]
-    newBoard[index] = turn
-    setBoard(newBoard)
-    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
-    setTurn(newTurn)
-
-    // Revisar si hay un ganador
-    const newWinner = checkWinner(newBoard)
-    if (newWinner){
-      setWinner(newWinner)
-    } else if (checkEndGame(newBoard)) {
-      setWinner(false)
-    }
-  }
   return (
-    <main className='board'>
+    <main className="board">
       <h1>Tic Tac Toe</h1>
+      <button className="resetButton" onClick={resetGame}>Reset Game</button>
       <section className="game">
         {
           board.map((_, index) => {
@@ -98,6 +74,7 @@ function App() {
           })
         }
       </section>
+
       <section className="turn">
         <Square isSelected={turn === TURNS.X}>
           {TURNS.X}
@@ -106,30 +83,31 @@ function App() {
           {TURNS.O}
         </Square>
       </section>
-      
+
       { 
         winner != null && (
           <section className="winner">
             <div className="text">
               <h2>
-                {
+                { /* Determinar si hubo un ganador o un empate */
                   winner === false ? 'Empate' : 'Ganó'
                 }
               </h2>
 
               <header className="win">
+                {/* Si tenemos un winner, lo renderizamos */}
                 {winner && <Square>{winner}</Square>}
               </header>
 
               <footer>
-                <button onClick={resetGame}>Empezar de nuevo</button>
+                <button className="resetButton" onClick={resetGame}>Empezar de nuevo</button>
               </footer>
             </div>
           </section>
         )
       }
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
