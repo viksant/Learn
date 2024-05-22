83425ff70118fbb7aec5fb9085f8b917e42d4c6f
VIKSANT <viksantviksant@gmail.com>
2024-05-22 20:09:39 +0200

    Install eslint

--- Myself/src/App.jsx
+++ Myself/src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { TURNS, WINNER_COMBOS, Square } from './Constants'
+import { TURNS, Square } from './Constants'
 import { checkWinner } from './Functions'
 import confetti from 'canvas-confetti'
 
